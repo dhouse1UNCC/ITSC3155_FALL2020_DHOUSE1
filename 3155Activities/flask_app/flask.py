fbf8c2de129369e3ec73fc3ec86e3fa99980527f
dhouse1UNCC <dhouse1@uncc.edu>
2020-10-27 18:35:55 +0000

    flask app initial commit

---
+++ 3155Activities/flask_app/flask.py
@@ -0,0 +1,14 @@
+# FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
+
+# imports
+from flask import Flask   # Flask is the web app that we will customize
+
+app = Flask(__name__)     # create an app
+
+# @app.route is a decorator. It gives the function "index" special powers.
+# In this case it makes it so anyone going to "your-url/" makes this function
+# get called. What it returns is what is shown as the web page
+@app.route('/')
+def index():
+    return 'Hello World!'
+
