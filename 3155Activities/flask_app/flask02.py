c94b6506aae1bf0330afdd30189e67e49f38c1f4
dhouse1UNCC <dhouse1@uncc.edu>
2020-10-29 23:13:44 +0000

    flask app #2 - created a dynamic welcome route

---
+++ 3155Activities/flask_app/flask02.py
@@ -0,0 +1,15 @@
+# FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
+
+# imports
+from flask import Flask   # Flask is the web app that we will customize
+from flask import render_template
+app = Flask(__name__)     # create an app
+
+# @app.route is a decorator. It gives the function "index" special powers.
+# In this case it makes it so anyone going to "your-url/" makes this function
+# get called. What it returns is what is shown as the web page
+@app.route('/index')
+def index():
+    a_user = {'name': 'Daniel House', 'email':'mogli@uncc.edu'}
+    return render_template('index.html', user = a_user)
+
