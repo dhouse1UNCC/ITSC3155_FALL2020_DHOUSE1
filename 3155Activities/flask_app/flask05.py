8d24a97774161a206702592f69f4b128c38c209a
dhouse1UNCC <dhouse1@uncc.edu>
2020-11-19 16:12:22 +0000

    flask app #5 - added route and function for edit

--- 3155Activities/flask_app/flask05.py
+++ 3155Activities/flask_app/flask05.py
@@ -63,3 +63,14 @@ def new_note():
     else:
         a_user = db.session.query(User).filter_by(email='dhouse1@uncc.edu').one()
         return render_template('new.html', user = a_user)
+
+@app.route('/notes/edit/<note_id>')
+def update_note(note_id):
+    # GET request - show new note form to edit note
+    # retrieve user from database
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
+
+    #retrieve note from database
+    my_note = db.session.query(Note). filter_by(id=note_id).one()
+
+    return render_template('new.html', note=my_note, user=a_user)
